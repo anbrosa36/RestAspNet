726ef7851638a335bc3a77903223ad7a56142e63
Andre Sousa <anbrosa36@gmail.com>
2024-11-07 18:45:25 -0300

    Implementado de Camadas

--- RestApplication/RestApplication/Business/Implematations/PersonBusinessImplementation.cs
+++ RestApplication/RestApplication/Business/Implematations/PersonBusinessImplementation.cs
@@ -1,85 +1,43 @@
 ﻿using RestApplication.Model;
 using RestApplication.Model.Context;
+using RestApplication.Repository;
 using System;
 
 namespace RestApplication.Business.Implematations
 {
     public class PersonBusinessImplementation : IPersonBusiness
     {
-        private BancoContext _context;
+        private readonly IPersonRepository _repository;
 
-        public PersonBusinessImplementation(BancoContext context)
+        public PersonBusinessImplementation(IPersonRepository repository)
         {
-            _context = context;
+            _repository = repository;
         }
 
         public List<Person> FindAll()
         {
-            return _context.Persons.ToList();
+            return _repository.FindAll();
         }
 
         public Person FindById(int id)
         {
-            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            return _repository.FindById(id);
         }
 
         public Person Create(Person person)
         {
-            try
-            {
-                _context.Add(person);
-                _context.SaveChanges();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-            return person;
+            return _repository.Create(person);
         }
 
         public Person Update(Person person)
         {
-            if (!Exists(person.Id)) return new Person();
 
-            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
-
-            if (result != null)
-            {
-                try
-                {
-                    _context.Entry(result).CurrentValues.SetValues(person);
-                    _context.SaveChanges();
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-            }
-
-            return person;
+            return _repository.Update(person);
         }
 
         public void Delete(int id)
         {
-            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
-
-            if (result != null)
-            {
-                try
-                {
-                    _context.Persons.Remove(result);
-                    _context.SaveChanges();
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-            }
-        }
-
-        private bool Exists(int id)
-        {
-            return _context.Persons.Any(p => p.Id.Equals(id));
+            _repository.Delete(id);
         }
     }
 }
