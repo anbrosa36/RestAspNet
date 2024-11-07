ed71027a386e54aad95c8d8a91e590162ba87c42
Andre Sousa <anbrosa36@gmail.com>
2024-11-06 23:05:00 -0300

    Implementado a conexão com o banco de dados e ajustando metodos

--- RestApplication/RestApplication/Services/IPersonService.cs
+++ RestApplication/RestApplication/Services/IPersonService.cs
@@ -5,9 +5,9 @@ namespace RestApplication.Services
     public interface IPersonService
     {
         Person Create (Person person);
-        Person FindById(long id);
+        Person FindById(int id);
         List<Person> FindAll();
         Person Update(Person person);
-        void Delete(long id);
+        void Delete(int id);
     }
 }
